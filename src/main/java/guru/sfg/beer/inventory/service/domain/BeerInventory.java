5b699a31053426fe61636a2b62bea684befca3c0
fnmabreu@gmail.com <fnmabreu@gmail.com>
2021-11-24 18:43:40 +0000

    Adding profile for MySQL

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -4,7 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -16,7 +18,14 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @Entity
-public class BeerInventory extends BaseEntity{
+public class BeerInventory extends BaseEntity {
+
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
+    private UUID beerId;
+
+    private String upc;
+    private Integer quantityOnHand = 0;
 
     @Builder
     public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
@@ -26,8 +35,4 @@ public class BeerInventory extends BaseEntity{
         this.upc = upc;
         this.quantityOnHand = quantityOnHand;
     }
-
-    private UUID beerId;
-    private String upc;
-    private Integer quantityOnHand = 0;
 }
